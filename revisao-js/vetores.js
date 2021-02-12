00c0e414971d58426c6f3a239f6db9c90023cf72
rodrigo02souza <rodrigo-souza02@hotmail.com>
2021-02-12 01:25:19 +0000

    (11/02) Inicio da revisao de JS

---
+++ revisao-js/vetores.js
@@ -0,0 +1,13 @@
+let frutas = ['maçã', 'laranja', 'pera', 'uva', 'mamão', 'banana']
+
+console.log(frutas)
+
+let ultimaFruta = frutas.pop()
+
+console.log(frutas)
+console.log(ultimaFruta)
+
+let terceiraFruta= frutas.splice(2, 1)
+
+console.log(frutas)
+console.log(terceiraFruta)
